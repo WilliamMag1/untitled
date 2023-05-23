3f707718d39404c01b886b774fd3c82ff2dda116
BT_3W2_03 <williamm534@nycstudents.net>
2023-05-23 12:17:59 -0400

    j

--- src/JavaHangman.java
+++ src/JavaHangman.java
@@ -28,12 +28,15 @@ public class JavaHangman {
                 "casting",
                 "abstraction",
                 "package",
-                "polymorphism",
                 "constructor",
                 "encapsulation",
                 "conditional",
                 "interface",
-                "exception"
+                "exception",
+                "boolean",
+                "byte",
+                "double",
+                "final"
         };
 
         hints = new String[] {
@@ -56,12 +59,16 @@ public class JavaHangman {
                 "Converting an object of one type to another type.",
                 "A concept of exposing only necessary information and hiding the implementation details.",
                 "A way to organize classes and interfaces into groups.",
-                "The ability of an object to take on many forms.",
                 "A special method used to initialize objects.",
                 "A mechanism of bundling data and methods together.",
                 "A control structure for making decisions based on conditions.",
                 "A contract specifying a set of methods that a class must implement.",
-                "An event that occurs during the execution of a program."
+                "An event that occurs during the execution of a program.",
+                "Refers to an expression or variable that can have only a true or false value. The Java programming language provides the boolean type and the literal values true and false.",
+                "A sequence of eight bits. Java provides a corresponding byte type",
+                "A Java keyword used to define a variable of type double.",
+                "A Java keyword. You define an entity once and cannot change it or derive from it later. More specifically: a final class cannot be subclassed, a final method cannot be overridden and a final variable cannot change from its initialized value."
+
         };
 
         score = 0;
